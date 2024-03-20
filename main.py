78414d4ca9ead6517effd932d1d740e234e7e0de
Mukto Akash <73410983+muktoakash@users.noreply.github.com>
2024-03-20 20:03:18 +0000

    Template

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 """./main.py"""
+
+# Import Modules
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
+
+# Main App Objects and Settings
+app = QApplication([])
+
+main_window = QWidget()
+main_window.setWindowTitle("Random Word Maker")
+main_window.resize(300, 200)
+
+# Create all App Objects
+
+# All Design Here
+
+# Events
+
+# Show/Run our App
+main_window.show()
+app.exec_()
