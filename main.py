3e81bf261a4ce2efd7e22fba2fa2ce8c493726d0
Mukto Akash <muktoakash@gmail.com>
2024-03-23 01:17:53 -0400

    Add background code for main_window

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 """./main.py"""
 
 #imports
+from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QPushButton, QHBoxLayout, QVBoxLayout, QGridLayout
 
 # App Settings
+app = QApplication([])
+main_window = QWidget()
+main_window.setWindowTitle("Calculator App")
+main_window.resize(250, 300)
 
 # All objects/Widgets
 
 # Design
 
 # Show/Run
+
+main_window.show()
+app.exec_()
