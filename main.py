da8e8b055d19e5dcef4eb477fa3b85b8d5b846ba
Mukto Akash <muktoakash@gmail.com>
2024-03-28 13:48:42 -0400

    Add buttons to third-app

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 """./main.py"""
 
+# Import Modules
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, \
     QPushButton, QListWidget, QComboBox, QHBoxLayout, QVBoxLayout
 from PyQt5.QtCore import Qt
 
+# App Settings
 app = QApplication([])
 main_window = QWidget()
 main_window.setWindowTitle("PhotoQt")
 main_window.resize(900, 700)
 
+# All app widgets/objects
+btn_folder = QPushButton("Folder")
+file_list = QListWidget()
 
+btn_left = QPushButton("Left")
+btn_right = QPushButton("Right")
+mirror = QPushButton("Mirror")
+sharpness = QPushButton("Sharpen")
+gray = QPushButton("Black/White")
+saturation = QPushButton("Color")
+contrast = QPushButton("Contrast")
+blur = QPushButton("Blur")
+
+# show 
 main_window.show()
 app.exec_()
