91249840ed57487cd00ea6cdb2d5e986c2885666
Mukto Akash <muktoakash@gmail.com>
2024-04-03 22:23:32 -0400

    Set Design of fourth-app

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from PyQt5.QtWidgets import QApplication, QWidget, QLabel, \
 class ExpenseApp(QWidget):
     """
     """
-    def _init__(self):
+    def __init__(self):
+        super().__init__()
     #Main App Objects & Settings
         self.resize(550, 500)
         self.setWindowTitle("Expense Tracker")
@@ -25,14 +26,38 @@ class ExpenseApp(QWidget):
         self.header_names = ['Id', 'date', 'Category', 'Amount', 'Description']
         self.table.setHorizontalHeaderLabels(self.header_names)
 
+    # Create Objects
 
+    # Design App with Layouts
         self.master_layout = QVBoxLayout()
         self.row1 = QHBoxLayout()
         self.row2 = QHBoxLayout()
         self.row3 = QHBoxLayout()
 
-    # Create Objects
+        self.row1.addWidget(QLabel("Date:"))
+        self.row1.addWidget(self.date_box)
+        self.row1.addWidget(QLabel("Category:"))
+        self.row1.addWidget(self.dropdown)
 
-    # Design App with Layouts
+        self.row2.addWidget(QLabel("Amount:"))
+        self.row2.addWidget(self.amount)
+        self.row2.addWidget(QLabel("Description:"))
+        self.row2.addWidget(self.description)
+
+        self.row3.addWidget(self.add_button)
+        self.row3.addWidget(self.delete_button)
+
+        self.master_layout.addLayout(self.row1)
+        self.master_layout.addLayout(self.row2)
+        self.master_layout.addLayout(self.row3)
+
+        self.master_layout.addWidget(self.table)
+
+        self.setLayout(self.master_layout)
 
 # Run the App
+if __name__ in "__main__":
+    app = QApplication([])
+    main = ExpenseApp()
+    main.show()
+    app.exec_()
