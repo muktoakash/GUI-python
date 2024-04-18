0bba4d0e8faa355ce23e514dcc5f707d91be85b1
Mukto Akash <73410983+muktoakash@users.noreply.github.com>
2024-04-18 14:01:49 +0000

    Add read_file functionality

--- Tkinter/main.py
+++ Tkinter/main.py
@@ -15,12 +15,16 @@ root.title("Demo Tkinter App")
 
 message = Label(root, text="Welcome to the Demo App")
 # Input
-entry_soft = StringVar()
-entry = Entry(open_program, textvariable=entry_soft)
+entry_soft = StringVar()    # Creates a Tkinter StringVar object 
+                            # to hold data for an Entry widget.
+entry = Entry(open_program, textvariable=entry_soft)    # Creates an Entry widget 
+                                                        # that will display/edit 
+                                                        # the text in entry_soft.
 # Button
 button = Button(open_program, text="Click Me!", command=read_file)
 # TextBox
-t1 = Text(open_program, height=2, width=23)
+text = Text(open_program, height=2, width=23)   # Creates a Text widget
+                                                # with specified height and width.
 
 
 # Layout
@@ -33,8 +37,15 @@ Tkinter has two main ways to organize widgets within the window — .pack() and 
 """
 message.pack()
 entry.grid(row=0, column=1) # align the label and input side-by-side
+text.grid(row=0, column=2)  # creates an Entry box and a Text box
+                            # side by side in the first row of the GUI
+
 #b1.pack() " this is one way to execute a button
 button.grid(row = 0, column = 0) #gives us more control
-t1.grid(row=0, column=2)
 
-#:
+
+# functionality (responding to events)
+def read_file():
+    value = float(entry_soft.get()) # .get() retrieves the content of a widget
+    text.insert(END, value) #alue of text would be inputted at the END and 
+                            #also takes the value of entry_soft
