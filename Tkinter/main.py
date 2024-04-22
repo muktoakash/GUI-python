12f5a8e6a83b842f99d86134195cf6c1d846d856
Mukto Akash <muktoakash@gmail.com>
2024-04-21 22:29:40 -0400

    Add scoreboard to tk-second-app

--- Tkinter/main.py
+++ Tkinter/main.py
@@ -8,8 +8,10 @@ the nltk.corpus at the press of buttons.
 from random import choice
 
 # imports
-from tkinter import Tk, Button, Label, messagebox,\
-     LabelFrame, END
+from tkinter import Button, Label, messagebox,\
+     LabelFrame
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
 
 # imports to use for words
 from nltk.corpus import wordnet as wn
@@ -17,6 +19,7 @@ from PyDictionary import PyDictionary
 
 # global constant:
 NUM_RANDOM_WORDS = 3 # currently only choosing three random words
+LEVEL = 0
 
 # Class for main app
 class RandomWords():
@@ -26,24 +29,33 @@ class RandomWords():
     def __init__(self):
         """Initialize"""
         # Main App Objects and Settings
-        self.root = Tk()
+        self.root = ttk.Window(themename="cyborg")
 
         # Title
         self.root.title("Vocab Show Down!")
 
         # Frame
-        self.game_frame = LabelFrame(self.root,
-                                     text="Show off your vocabulary!",
-                                     padx=50, pady=50)
+        self.game_frame = LabelFrame(self.root, \
+                                     text="Show off your vocabulary!", \
+                                     padx=5, pady=5, \
+                                     font=("Helvetica", 15))
 
         # Create all App Objects
-        self.text1 = Button(self.game_frame, text="?", height=2, width=30, \
-                            command=lambda : self.check_answer(0))
-        self.text2 = Button(self.game_frame, text="?", height=2, width=30, \
-                            command=lambda : self.check_answer(1))
-        self.text3 = Button(self.game_frame, text="?", height=2, width=30, \
-                            command=lambda : self.check_answer(2))
-        self.give_up = Button(self.game_frame, text="Pass!", command = self.pass_round, height=3, width=60)
+        self.style_info = ttk.Style()
+        self.style_info.configure('info.TButton', font=('Helvetica', 15))
+        self.text1 = ttk.Button(self.game_frame, text="?", width=30, \
+                            command=lambda : self.check_answer(0),
+                            style='info.TButton')
+        self.text2 = ttk.Button(self.game_frame, text="?", width=30, \
+                            command=lambda : self.check_answer(1),
+                            style='info.TButton')
+        self.text3 = ttk.Button(self.game_frame, text="?", width=30, \
+                            command=lambda : self.check_answer(2),
+                            style='info.TButton')
+
+        self.give_up = ttk.Button(self.game_frame, text="Pass!",
+                                command = self.pass_round,
+                                width=60, bootstyle='light',)
 
         self.random_words = [self.text1, self.text2, self.text3]
 
@@ -53,18 +65,56 @@ class RandomWords():
         # Game variables
         self.answer = "?"
         self.parts_of_speech = "?"
-        self.meaning = Label(self.game_frame, text="?", height=5, width=100, wraplength=700)
+        self.meaning = Label(self.game_frame, text="?",
+                             height=5, width=100,
+                              wraplength=700, font=("Helvetica", 15))
         self.correct = 0
         self.wrong = 0
         self.passed = 0
+        self.exp_req = LEVEL+1 *
+
+        # Top objects:
+        self.score_book = ttk.Label(self.root,
+                                    text = "Current Score: " \
+                                        + str(self.get_score()),
+                                    font = ("Courier", 15),
+                                    bootstyle='primary')
+
+        self.score_frame = ttk.LabelFrame(self.root,
+                                          text = "Score Book",
+                                          bootstyle = 'primary')
+
+        self.num_correct = ttk.Label(self.score_frame,
+                                     text = "Correct Answers: " \
+                                     + str(self.correct),
+                                     font = ("Courier", 12))
+
+        self.num_wrong = ttk.Label(self.score_frame,
+                                     text = "Wrong Answers: " \
+                                     + str(self.wrong),
+                                     font = ("Courier", 12))
+
+        self.num_passed = ttk.Label(self.score_frame,
+                                     text = "Passed: " \
+                                          + str(self.passed),
+                                     font = ("Courier", 12))
+
 
         # Layout
-        self.game_frame.pack(padx=5, pady=5)
+        # self.root.geometry("800x600")
+        self.score_book.pack(padx=15, pady=15)
+
+        self.score_frame.pack(fill=X, padx=15, pady=15,)
+        self.num_correct.grid(row=0, column=0, padx=100, sticky='nsew')
+        self.num_wrong.grid(row=0, column=1, padx=100, sticky='nsew')
+        self.num_passed.grid(row=0, column=2, padx=100, sticky='nsew')
+
+        self.game_frame.pack(padx=5, pady=5, fill=X)
         self.meaning.grid(row=0, column=0, columnspan=3, sticky='E'+'W',)
         for index in range(NUM_RANDOM_WORDS):
             self.random_words[index].grid(row = 1, column = index)
 
-        self.give_up.grid(row=3, column=1, padx=10, pady=10)
+        self.give_up.grid(row=3, column=1, pady=15)
 
         # Initialize
         self.play_game()
@@ -130,6 +180,13 @@ class RandomWords():
                                 f'{self.answer} - \n' + \
                                 f'{self.parts_of_speech} : {self.meaning["text"]}',
                                 icon = 'error')
+
+        self.score_book["text"] = "Current Score: " \
+                                + str(self.get_score())
+
+        self.num_correct["text"] = "Correct Answers: " + str(self.correct)
+        self.num_wrong["text"] = "Wrong Answers: " + str(self.wrong)
+
         if response:
             self.play_game()
         else:
@@ -137,7 +194,11 @@ class RandomWords():
 
     def pass_round(self):
         self.passed += 1
+        self.num_passed["text"] = "Passed: " + str(self.passed)
         self.play_game()
+
+    def get_score(self):
+        return 5 * self.correct - 2 * self.wrong
 
 # Show/Run our App
 if __name__== "__main__":
