029b6b7935dddd29c22d0babde5218e89278200f
Mukto Akash <muktoakash@gmail.com>
2024-07-01 22:53:07 -0400

    Complete StackLayoutExample

--- kivy/main.py
+++ kivy/main.py
@@ -7,6 +7,17 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
+from kivy.metrics import dp
+
+class StackLayoutExample(StackLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = "lr-bt"
+        for i in range(10):
+            size = dp(100) + i * 10
+            b = Button(text = str(i+1), size_hint = (None, None), size = (size, size))
+            self.add_widget(b)
 
 # class GridLayourExample(GridLayout):
 #     pass
