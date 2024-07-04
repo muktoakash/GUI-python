5993e521fa699652de07e2ac8a517072bb142ebb
Mukto Akash <muktoakash@gmail.com>
2024-07-03 23:56:29 -0400

    Implement ScrollView for kv-first-app

--- kivy/main.py
+++ kivy/main.py
@@ -13,9 +13,10 @@ from kivy.metrics import dp
 class StackLayoutExample(StackLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.orientation = "lr-bt"
-        for i in range(10):
-            size = dp(100) + i * 10
+        # self.orientation = "lr-bt"
+        for i in range(100):
+            # size = dp(100) + i * 10
+            size = dp(100)
             b = Button(text = str(i+1), size_hint = (None, None), size = (size, size))
             self.add_widget(b)
 
