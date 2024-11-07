bf3b297ab8b4d46c9aa4f2c4e4b9392cb9c86e91
Mukto Akash <muktoakash@gmail.com>
2024-11-07 00:34:46 -0500

    Use BooleanProperty to disable button

--- kivy/main.py
+++ kivy/main.py
@@ -9,12 +9,12 @@ from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.metrics import dp
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, BooleanProperty
 
 class WidgetsExample(GridLayout):
     """"""
     count = 0
-    count_enabled = False
+    count_enabled = BooleanProperty(False)
     my_text = StringProperty("Hello!")
 
     def on_button_click(self):
